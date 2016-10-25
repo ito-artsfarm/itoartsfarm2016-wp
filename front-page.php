77ab138d267b1d46281fee263850af2b51e2b469
makizonokenji <if.moshizono@gmail.com>
2016-10-25 22:11:08 +0900

    石田大祐サウンドパフォーマンスのURLを追加しました！

--- front-page.php
+++ front-page.php
@@ -175,7 +175,7 @@
       <h3><a href="http://www.ito-artsfarm.com/2016/09/12/event_dominick/">ドミニク・チェン氏スペシャルトークセッション<br>
       「発酵から学ぶコモンズ(共有財)ーこれからの生の価値とアート」</a></h3>　
       16:00〜16:30
-      <h3>石田大祐パフォーマンス</h3>　
+      <h3><a href="http://www.ito-artsfarm.com/2016/10/25/4524/">石田大祐サウンドパフォーマンス</a></h3>　
       16:00〜17:00
       <h3><a href="http://www.ito-artsfarm.com/2016/10/25/event_rob/">Rob Feulnerキュレーション特別上映プログラム「Glitch analogique du Nord」</a></h3>
     </div>
